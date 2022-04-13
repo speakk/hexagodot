4bc502ce49c3ba30bc61cc242f0e50cde012d9db
speakk <speakk@gmail.com>
2022-04-13 08:45:01 +0300

    Tweening next scene in

--- SceneManager.gd
+++ SceneManager.gd
@@ -16,15 +16,25 @@ func switch_scenes(sceneName):
   var to_scene = load("res://MainScenes/%s.tscn" % sceneName).instance()
   
   var next_index = 1 if current_index == 0 else 0
-  var nextViewportContainer = get_child(next_index)
-  nextViewportContainer.get_node("Viewport").add_child(to_scene)
+  var nextViewportContainer = $Containers.get_child(next_index)
+  var nextViewportNode = nextViewportContainer.get_node("Viewport")
+  nextViewportNode.add_child(to_scene)
+  var width = ProjectSettings.get_setting("display/window/size/width")
+  print("width", width)
   nextViewportContainer.visible = true
   
- 
-  var currentViewportContainer = get_child(current_index)
-  currentViewportContainer.visible = false
+  var currentViewportContainer = $Containers.get_child(current_index)
   var current_scene = currentViewportContainer.get_node("Viewport").get_child(0)
+  
+  var tweenLength = 0.3
+  $Tween.interpolate_property(nextViewportContainer, "rect_position", Vector2(width, 0), Vector2(0, 0), tweenLength)
+  $Tween.interpolate_property(currentViewportContainer, "rect_position", Vector2(0, 0), Vector2(-width, 0), tweenLength)
+  $Tween.start()
+  
+  yield($Tween, "tween_all_completed")
+  
   if current_scene:
+    currentViewportContainer.visible = false    
     current_scene.queue_free()
     
   nextViewportContainer.raise()
